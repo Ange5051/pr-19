8ed8a06ea0c173ec75889e9bf42129b5632c04ed
Ange5051 <147865401+Ange5051@users.noreply.github.com>
2025-01-28 13:54:16 +0200

    Add files via upload

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Books
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public int Year { get; set; }
+    public int Pages { get; set; }
+
+    public void Info()
+    {
+        Console.WriteLine($"Название: {Title}");
+        Console.WriteLine($"Автор: {Author}");
+        Console.WriteLine($"Год выпуска: {Year}");
+        Console.WriteLine($"Объем в листах: {Pages}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Books book = new Books
+        {
+            Title = "Я такой как все",
+            Author = "Олег Тиньков",
+            Year = 2020,
+            Pages = 374
+        };
+
+        book.Info();
+        Console.ReadKey();
+    }
+}
